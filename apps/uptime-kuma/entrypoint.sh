15dcc5f1b1f290ce1a920da22e91eb3b404b2d57
christfried <christfriedbalizou@gmail.com>
2025-10-27 22:15:10 +0000

    change: [uptime-kuma] clean docker file and entrypopint

--- apps/uptime-kuma/entrypoint.sh
+++ apps/uptime-kuma/entrypoint.sh
@@ -2,20 +2,14 @@
 
 set -e
 
-# Environment defaults (PUID/PGID can be passed via -e when running the container)
 PUID=${PUID:-0}
 PGID=${PGID:-0}
 
 log() {
-    # Simple logger used for consistent output formatting
     printf "%s\n" "==> $*"
 }
 
 files_ownership() {
-    # -h : change the ownership of symbolic links themselves
-    # -R : recursive
-    # -c : like verbose but report only when a change is made
-    # Uptime Kuma stores its data in /app/data
     log "Changing ownership of /app/data to ${PUID}:${PGID}"
     chown -hRc "${PUID}":"${PGID}" /app/data
 }
@@ -25,10 +19,4 @@ files_ownership
 
 log "Starting application with user ${PUID} group ${PGID}"
 
-if command -v setpriv >/dev/null 2>&1; then
-    exec setpriv --reuid "${PUID}" --regid "${PGID}" --clear-groups "$@"
-fi
-
-if [ -x /usr/bin/dumb-init ]; then
-    exec /usr/bin/dumb-init -- "$@"
-fi
+exec setpriv --reuid "${PUID}" --regid "${PGID}" --clear-groups "$@"
